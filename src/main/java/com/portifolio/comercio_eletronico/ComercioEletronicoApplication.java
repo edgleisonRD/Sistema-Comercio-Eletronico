829f6ecbaaa8c333990c3207239e27eb245cfdff
Edgleison RD <edgleisontic@gmail.com>
2026-03-05 19:21:38 -0300

    primeiro commit

---
+++ src/main/java/com/portifolio/comercio_eletronico/ComercioEletronicoApplication.java
@@ -0,0 +1,13 @@
+package com.portifolio.comercio_eletronico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComercioEletronicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComercioEletronicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portifolio/comercio_eletronico/ComercioEletronicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portifolio.comercio_eletronico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComercioEletronicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
