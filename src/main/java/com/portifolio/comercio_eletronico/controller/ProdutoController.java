d879c6c5a235093ffe3162f859d624cdea77f761
Edgleison RD <edgleisontic@gmail.com>
2026-03-24 23:05:33 -0300

    Buscar produtos com paginação

--- src/main/java/com/portifolio/comercio_eletronico/controller/ProdutoController.java
+++ src/main/java/com/portifolio/comercio_eletronico/controller/ProdutoController.java
@@ -1,14 +1,15 @@
 package com.portifolio.comercio_eletronico.controller;
 
 import com.portifolio.comercio_eletronico.dto.ProdutoDTO;
-import com.portifolio.comercio_eletronico.model.Produto;
-import com.portifolio.comercio_eletronico.repositorios.ProdutoRepositorio;
 import com.portifolio.comercio_eletronico.servico.ProdutoServico;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RestController
@@ -28,6 +29,10 @@ public class ProdutoController {
         // @PathVariable diz ao Spring: "pegue o número que está na URL e coloque na variável 'id'".
 
         // O Controller chama o Serviço para buscar o dado e já o retorna como DTO.
-        return produtoServico.fitrarId(id);
+        return produtoServico.fitrarPorId(id);
+    }
+    @GetMapping
+    public Page<ProdutoDTO> filtrarTodos(Pageable pageable) {
+        return  produtoServico.filtrarTodos(pageable);
     }
 }
--- src/main/java/com/portifolio/comercio_eletronico/servico/ProdutoServico.java
+++ src/main/java/com/portifolio/comercio_eletronico/servico/ProdutoServico.java
@@ -3,8 +3,12 @@ package com.portifolio.comercio_eletronico.servico;
 import com.portifolio.comercio_eletronico.dto.ProdutoDTO;
 import com.portifolio.comercio_eletronico.model.Produto;
 import com.portifolio.comercio_eletronico.repositorios.ProdutoRepositorio;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service // Indica ao Spring que esta classe é um "Bean" de serviço, onde fica a lógica de negócio.
 public class ProdutoServico {
@@ -17,17 +21,16 @@ public class ProdutoServico {
     public ProdutoServico(ProdutoRepositorio repositorio) {
         this.repositorio = repositorio;
     }
-    // Define que o método é uma transação de banco de dados.
-    // 'readOnly=true' otimiza a performance, avisando que você só vai ler dados, não vai alterar nada.
+
     @Transactional(readOnly=true)
-    public ProdutoDTO fitrarId(Long id) {// Busca o produto no banco pelo ID.
-
-        // O .get() extrai o objeto de dentro do 'Optional' retornado pelo Spring Data.
+    public ProdutoDTO fitrarPorId(Long id) {// Busca o produto no banco pelo ID.
         Produto produto = repositorio.findById(id).get();
-
-        // Converte a entidade 'Produto' (do banco) para um 'ProdutoDTO' (objeto de transferência).
-        // Isso evita expor detalhes internos do banco para o usuário final.
         return new ProdutoDTO(produto);
+    }
+    @Transactional(readOnly=true)
+    public Page<ProdutoDTO> filtrarTodos(Pageable pageable) {
+        Page<Produto> result = repositorio.findAll(pageable);
+        return result.map(x -> new ProdutoDTO(x));
     }
 }
 
